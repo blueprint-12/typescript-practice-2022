e83f313e868e3b652bb85a161e06c2dbb76b086d
blueprint-12 <blueprint12@daum.net>
2022-12-22 22:13:44 +0900

    TS syntax 심화(~ any, unknown 타입 대입 가능 표) all ref: zero cho님

--- zerocho-lect/test.ts
+++ zerocho-lect/test.ts
@@ -164,3 +164,140 @@ if (head) {
   type O = { hello: "world" } & { zero: "cho" };
   const b: O = { hello: "world", zero: "cho" };
 }
+
+{
+  // 타입을 &과 사용하여 상속처럼 사용 가능
+  type Animal = { breath: true };
+  type Poyouryu = Animal & { breed: true };
+  type Human = Poyouryu & { think: true };
+
+  const cong: Human = { breath: true, breed: true, think: true };
+
+  interface A {
+    think: true;
+  }
+  interface B extends A {
+    breed: true;
+  }
+  const b: B = {
+    breed: true,
+    think: true,
+  };
+}
+
+{
+  //인터페이스 특징: 확장성, 중복 선언 가능(동일한 인터페이스에 프로퍼티가 추가된다.)
+  interface A {
+    talk: () => void;
+  }
+  interface A {
+    sleep: () => void;
+  }
+  interface A {
+    love: () => void;
+  }
+  interface A {
+    eat: () => void;
+  }
+  const a: A = { talk() {}, eat() {}, love() {}, sleep() {} };
+}
+
+{
+  //좁은 타입과 넓은 타입
+  //좁은 타입을 넓은 타입에 넣는 것(대입)은 가능
+  //하지만, 넓은 타입을 좁은 타입에는 X 집합을 예시로 생각하면 된다.
+  // ->직관적으로 헷갈리는 타입을 그냥 다른 타입으로 넣어봤을 때,
+  // 오류가 안뜬다면 좁은 타입인거고 오류가 뜬다면 넓은 타입을 좁은 타입에 넣은 것.
+  type A = string | number;
+  type B = string;
+
+  type C = string & number;
+
+  //객체는 상세적(구체적)일수록 좁은 범위라고 생각하면 된다.
+  type ObjA = { name: string };
+  type ObjB = { age: number };
+  type ObjAB = ObjA | ObjB;
+  type ObjC = ObjA & ObjB;
+  const ab: ObjAB = {
+    age: 28,
+  };
+
+  //예외적인 case, 타입이 넓냐 좁냐 검사 외에도 '잉여속성 검사'를 하는데
+  //객체 리터럴일 때 바로 집어넣어서 타입 사이즈 체크를 하면 헷갈릴 수 있다.
+  //(좁은 타입을 넓은 타입에 넣은 건데 왜 안되지? 싶은 순간)
+  //이럴 때는 중간에 다른 객체로 빼주기만 해도 에러가 사라진다.
+  const objC: ObjC = {
+    age: 28,
+    name: "cong",
+    // married: false, 여기서 객체 리터럴이라 바로 에러가 난다.(컨텐츠를 그대로 대입하는 방법)
+  };
+  //obj라는 객체로 따로 빼주어 대입하면 에러가 뜨지 않는다.
+  const obj = { name: "cong", age: 28, married: true };
+  const objC1: ObjC = obj;
+}
+{
+  //void 타입
+  //따로 빠져있는 함수의 void + 매개변수로 선언된 함수(콜백)의 void
+  function a(callback: () => void): void {
+    return;
+  }
+
+  //메서드로서의 void
+  interface Human {
+    talk: () => void;
+  }
+  const human: Human = {
+    talk() {
+      return 123;
+    },
+  };
+}
+
+//이전에 함수도 body(구현부)없이 선언할 수 있다고 했는데
+//대신 구현부와 선언부를 나눠놓은 것이기 떄문에 바로 밑에 구현부를 써줘야 한다.
+declare function forEach(arr: number[], callback: (el: number) => void): void;
+//구현부를 만들 기 싫을때는 선언부 앞에 declare를 붙여주면 된다.-> 실제 JS파일에서는 사라진다.
+let target: number[] = [];
+forEach([1, 2, 3], (el) => target.push(el));
+
+{
+  interface A {
+    talk: () => void;
+  }
+  const a: A = {
+    talk() {
+      return 3;
+    },
+  };
+  //any는 타입선언을 포기해버리는 것
+  //unknown은 "지금 당장은 타입을 정확히 모를 때" 사용하는 것, 직접 쓸 때 타입을 as로 정의해주면 된다.
+  const b: unknown = a.talk();
+  (b as A).talk();
+}
+{
+  try {
+  } catch (error) {
+    //대표적으로 error의 타입은 unknown이다.
+    //Error는 TS에서 제공하는 기본 error타입, Axios error의 경우 AxiosError로 명시해줘야 한다.
+    (error as Error).message;
+  }
+}
+{
+  interface A {
+    talk: () => void;
+  }
+
+  const a: A = {
+    talk() {
+      return 3;
+    },
+  };
+  const b = a.talk() as unknown as number; //talk가 void로 잘못선언되어있기 때문에 강제형변환해야함
+  b.toString(); //만약에 위에 as unknown as number가 없어서 TS문법 오류가 나도 js 로 컴파일이 안되는 것은 아님
+}
+
+{
+  function a(): void {
+    return undefined;
+  }
+}
